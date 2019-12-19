b14e69b4c4b078abef28ab63abe6803f588ce796
linzhongli-0419 <56338980+linzhongli-0419@users.noreply.github.com>
2019-12-19 12:20:16 +0800

    Create code.1.java

---
+++ code.1.java
@@ -0,0 +1,8 @@
+public class Test13 {
+    public static void main(String[] args) {
+        System.out.println("姓名，张三");
+        System.out.println("性别，男");
+        System.out.println("年龄，18");
+        System.out.println("出生年月，1994.8.30");
+    }
+}
