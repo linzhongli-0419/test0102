d1516a47a758d53b12adc356cd55143dc144cce7
linzhongli-0419 <56338980+linzhongli-0419@users.noreply.github.com>
2020-01-04 16:41:56 +0800

    Update a.java

--- a.java
+++ a.java
@@ -1,6 +1,6 @@
 public class Test13 {
     public static void main(String[] args) {
-        System.out.println("姓名，张三");
+        System.out.println("姓名，张三去");
         System.out.println("性别，男");
         System.out.println("年龄，11");
         System.out.println("出生年月，1994.8.30");
