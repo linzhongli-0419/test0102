01f024a83f0c40ab64488bbe712d4cc6b921b99a
linzhongli-0419 <56338980+linzhongli-0419@users.noreply.github.com>
2020-03-11 19:43:36 +0800

    测试
    描述啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊冷静

--- a.java
+++ a.java
@@ -1,7 +1,7 @@
 public class Test13 {
     public static void main(String[] args) {
         System.out.println("姓名，张三");
-        System.out.println("性，男");
+        System.out.println("xinbbie，男");
         System.out.println("年龄，11");
         System.out.println("出生年月，1994.8.30");
     }
