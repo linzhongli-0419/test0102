aab7eae485bfc993fc29b66ec7bff605ca9c0357
林中莉的昵称 <56338980+linzhongli-0419@users.noreply.github.com>
2020-03-14 11:24:21 +0800

    #1

--- code.1.java
+++ code.1.java
@@ -1,7 +1,7 @@
 public class Test13 {
     public static void main(String[] args) {
         System.out.println("姓名，李四");
-        System.out.println("性别，男");
+        System.out.println("别，男");
         System.out.println("年龄，211");
         System.out.println("出生年月，1994.8.10");
     }
