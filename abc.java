76f681d3c8f3e7047dda35026100c7e63ef3ee55
林中莉的昵称 <56338980+linzhongli-0419@users.noreply.github.com>
2020-05-09 13:45:08 +0800

    #2 chok chok

--- abc.java
+++ abc.java
@@ -2,7 +2,7 @@ public class Test13 {
     public static void main(String[] args) {
         System.out.println("姓名，张三");
         System.out.println("性别，ren");
-        System.out.println("年龄，111");
+        System.out.println("年龄，11");
         System.out.println("出生年月，1994.8.30");
     }
 }
